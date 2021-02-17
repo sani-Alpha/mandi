19d297bc50a8f09cf12f758be0bd4f8a93ba1f49
sani-Alpha <rai.sanidhya2010@gmail.com>
2021-02-17 14:26:35 +0530

    Added front page and its supporting docs

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="main is-primary">
+      <div class="main-body">
+        <div class="container">
+          <h1 class="title is-size-1">About Mandi</h1>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <p class="mandi-desc is-size-4">Desc of app goes here</p>
+      <p class="mandi-desc is-size-4">para 2 of Desc of app goes here</p>
+    </div>
   </div>
 </template>
+<style lang="scss" scoped>
+  .mandi-desc {
+    margin-top: 50px;
+  }
+</style>
